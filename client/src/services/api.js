5b2a8e6537b158a39e45c0955d626d80e884252d
shreyas-patil-31 <shreyasrjpatil99@gmail.com>
2026-07-17 16:01:57 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-code-reviewer-74ec.onrender.com",
 });
 
 export const reviewCode = async (language, code) => {
